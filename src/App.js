10e05cf00dfb22824fbfa7496481826a8739761b
YANICK  NDI <Yanick-web@users.noreply.github.com>
2021-04-21 17:56:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+
+
+const Todo = ({todo, todos, setTodos})=>{
+
+
+const handleDelete =()=>{
+setTodos(todos.filter((el) => el.id !== todo.id));
+};
+
+const handleDone = ()=>{
+  setTodos(todos.map((el)=> {
+    if(el.id === todo.id){
+      return {...el, done: !el.done};
+    }
+    return el;
+  }))
+};
+  return(
+    <div id="todo_item" className={todo.done? "cross": ""}>
+    <li>{todo.text}</li>
+    <div id="control_buttons">
+    <button id="mark" onClick={handleDone}>Mark As Done</button>
+    <button id="delete" onClick={handleDelete}>Delete</button>
+    </div>
+    </div>
+  );
+};
+
 function App() {
+
+
+//STATE
+const [input, setInput] = useState("");
+const [todos, setTodos] = useState([]);
+const [filter, setFilter] = useState("all");
+const [filtered, setFiltered] = useState([]);
+
+
+//HANDLERS
+const handleInput = (e)=>{
+  setInput(e.target.value);
+};
+
+
+
+const handleSubmit = (e)=>{
+  e.preventDefault();
+  if(input === ""){
+window.alert("Input is empty. Please enter a todo");
+  }else{
+  setTodos([
+    ...todos,
+    {id: Math.floor(Math.random()*1001), text: input, done: false}
+  ]);
+
+  setInput("");
+}
+};
+
+  const handleFilter = (e)=>{
+  setFilter(e.target.value);
+};
+
+
+const filterItems = () => {
+  switch(filter){
+    case "completed":
+      setFiltered(todos.filter(el => el.done === true));
+      break;
+    case "uncompleted":
+      setFiltered(todos.filter(el => el.done === false));
+      break;
+    default:
+      setFiltered(todos);
+      break;
+  }
+}
+
+ useEffect(()=>{
+getLocalItems()
+  }, []);
+
+
+  useEffect(() => {
+    saveToLocal()
+    filterItems();
+
+  }, [ filter, todos]);
+
+
+  //SAVE AND RETRIEVE FROM LOCAL STORAGE
+  const saveToLocal = ()=>{
+    if(localStorage.getItem("todolist") === null){
+      localStorage.setItem("todolist", JSON.stringify([]));
+    }else{
+      localStorage.setItem("todolist", JSON.stringify(todos));
+    }
+  }
+ 
+  const getLocalItems = ()=> {
+   let items = JSON.parse(localStorage.getItem("todolist"));
+   setTodos(items);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="todo_text">Enter a todo</label>
+      <input type="text" name="todo_text" value={input} onChange={handleInput}/>
+      <input type="submit" value="Add(+)" />
+
+      <select id="filters" name="filters" onChange={handleFilter}>
+        <option value="all" defaultValue>All</option>
+        <option value="completed" >Completed</option>
+        <option value="uncompleted" >Uncompleted</option>
+      </select>
+    </form>
+
+    <ul>
+      {filtered.map((todo, i) => <Todo key={todo.id.toString()} todo={todo} todos= {todos} setTodos={setTodos}/>)}
+    </ul>
     </div>
   );
 }
